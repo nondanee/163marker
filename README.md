1af7266ab0b828f4d1a7ef89d9e0d606419e1aab
Nzix <iminezn5656@outlook.com>
2019-06-26 18:29:01 +0800

    support flac

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # 163 Marker
 
+通过用户动态分享历史数据为已经 "消失" 的歌曲构造 `163 key(Don't modify)` 标记
+
+让非网易云客户端下载的音频文件能被客户端识别，正常匹配歌词和查看评论
+
+## Dependency
+
+```
+$ pip install -r requirements.txt
+```
+
 ## Usage
 
 ```
-pip install -r requirements.txt
-python main.py [file] [url]
+$ python main.py [file] [url]
 ```
+
+- `file` 支持 MP3 和 FLAC 格式
+
+- `url` 用户动态链接 (客户端内分享动态，复制链接)
--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 # -*- coding: utf-8 -*-
 import sys
-import re, json, binascii, base64, hashlib, html
+import re, json, binascii, base64, hashlib
 
 import requests
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from mutagen import mp3, flac, id3
 
-meta_key = binascii.a2b_hex('2331346C6A6B5F215C5D2630553C2728')
+key = binascii.a2b_hex('2331346C6A6B5F215C5D2630553C2728')
 
-def parse_event(url):
+def parser(url):
     id = re.search(r'id=(\d+)', url)[1]
     uid = re.search(r'uid=(\d+)', url)[1]
     response = requests.get('https://music.163.com/event', params = {'id': id, 'uid': uid})
     data = re.search(r'<textarea.+id="event-data".*>([\s\S]+?)</textarea>', response.text)[1]
-    data = json.loads(html.unescape(data))
+    data = json.loads(data.replace('&quot;', '"'))
     song = json.loads(data['json'])['song']
     return song
 
 def marker(path, song):
-    def file_stream(file):
+    def streamify(file):
         with file:
             return file.read()
 
-    def embed_cover(item, content, type):
+    def embed(item, content, type):
         item.encoding = 0
         item.type = type
         item.mime = 'image/png' if content[0:4] == binascii.a2b_hex('89504E47') else 'image/jpeg'
         item.data = content
+
+    format = 'flac' if open(path, 'rb').read(4) == binascii.a2b_hex('664C6143') else 'mp3'
     
     meta = {
         'album': song['album']['name'],
@@ -40,33 +42,46 @@ def marker(path, song):
         'musicName': song['name'],
         'mvId': song['mvid'],
         'transNames': [],
-        'format': 'mp3'
+        'format': format
     }
 
-    audio = mp3.EasyMP3(path)
+    audio = mp3.EasyMP3(path) if format == 'mp3' else flac.FLAC(path)
     meta['bitrate'] = audio.info.bitrate
     meta['duration'] = (int(audio.info.length * 1000))
-    meta['mp3DocId'] = hashlib.md5(file_stream(open(path, 'rb'))).hexdigest()
+    meta['mp3DocId'] = hashlib.md5(streamify(open(path, 'rb'))).hexdigest()
 
-    cryptor = AES.new(meta_key, AES.MODE_ECB)
+    cryptor = AES.new(key, AES.MODE_ECB)
     identification = 'music:' + json.dumps(meta)
     identification = '163 key(Don\'t modify):' + base64.b64encode(cryptor.encrypt(pad(identification.encode('utf8'), 16))).decode('utf8')
 
+    audio.delete()
     audio['title'] = meta['musicName']
     audio['album'] = meta['album']
     audio['artist'] = '/'.join([artist[0] for artist in meta['artist']])
-    audio.tags.RegisterTextKey('comment', 'COMM')
-    audio['comment'] = identification
+
+    if format == 'flac':
+        audio['description'] = identification
+    else:
+        audio.tags.RegisterTextKey('comment', 'COMM')
+        audio['comment'] = identification
     audio.save()
 
-    audio = mp3.MP3(path)
-    image = id3.APIC()
-    embed_cover(image, requests.get(meta['albumPic'] + '?param=300y300').content, 6)
-    audio.tags.add(image)
+    image_data = requests.get(meta['albumPic'] + '?param=300y300').content
+    if format == 'flac':
+        audio = flac.FLAC(path)
+        image = flac.Picture()
+        embed(image, image_data, 3)
+        audio.clear_pictures()
+        audio.add_picture(image)
+    elif format == 'mp3':
+        audio = mp3.MP3(path)
+        image = id3.APIC()
+        embed(image, image_data, 6)
+        audio.tags.add(image)
     audio.save()
 
 if __name__ == '__main__':
     try:
-        marker(sys.argv[1], parse_event(sys.argv[2]))
+        marker(sys.argv[1], parser(sys.argv[2]))
     except Exception as e:
         print(e)
